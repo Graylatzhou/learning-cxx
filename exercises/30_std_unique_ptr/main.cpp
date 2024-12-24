d36aae7fd5b9a9410dfc558e7707b6346b0267a7
Graylatzhou <1391087899@qq.com>
2024-12-24 19:12:36 +0800

    debug q30

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -67,6 +67,8 @@ int main(int argc, char **argv) {
     for (auto i = 0; i < 3; ++i) {
         ASSERT(problems[i].size() == answers[i].size(), "wrong size");
         for (auto j = 0; j < problems[i].size(); ++j) {
+            std::cout << problems[i][j].c_str() << std::endl;
+            std::cout << answers[i][j] << std::endl;
             ASSERT(std::strcmp(problems[i][j].c_str(), answers[i][j]) == 0, "wrong location");
         }
     }
